bb9807f673d7ffa68c7dc2b042abe68d804a9ea9
Roman Nikonorov <roman@rnikonorov.ru>
2023-04-14 22:13:31 +0700

    code clean up

--- Rest2Kafka/messages.go
+++ Rest2Kafka/messages.go
@@ -2,13 +2,13 @@ package main
 
 import "github.com/Shopify/sarama"
 
-func prepareMessage(message *MessageStructure, topic string) (*sarama.ProducerMessage) {
+func prepareMessage(message *MessageStructure, topic string) *sarama.ProducerMessage {
 	messageString := string(message.Message)
 	producerMessage := &sarama.ProducerMessage{Topic: topic, Value: sarama.StringEncoder(messageString)}
 	return producerMessage
 }
 
-func prepareMessages(messages *[]MessageStructure, topic string) ([]*sarama.ProducerMessage) {
+func prepareMessages(messages *[]MessageStructure, topic string) []*sarama.ProducerMessage {
 	result := make([]*sarama.ProducerMessage, len(*messages))
 	for i, message := range *messages {
 		result[i] = prepareMessage(&message, topic)
--- Rest2Kafka/rest2kafka.go
+++ Rest2Kafka/rest2kafka.go
@@ -15,23 +15,32 @@ var (
 	userName = flag.String("username", "", "The SASL username")
 	passwd   = flag.String("passwd", "", "The SASL password")
 	brokers  = flag.String("brokers", "localhost:9092", "The Kafka brokers to connect to, as a comma separated list")
-	//batchSize = flag.Int64("batch", 100, "Producer batch size")
+    encription = flag.String("encription", "256", "Encription mode 256 or 512")
 )
 
 func processMessages(messages *[]MessageStructure, responseWriter *http.ResponseWriter, topic string) {
-	//jsonEncoder := json.NewEncoder(*responseWriter)
-	conf := sarama.NewConfig()
-	conf.Producer.Retry.Max = 1
-	conf.Producer.RequiredAcks = sarama.WaitForAll
-	conf.Producer.Return.Successes = true
-	conf.Metadata.Full = true
-	conf.Version = sarama.V0_10_0_0
-	conf.Net.SASL.Enable = true
-	conf.Net.SASL.User = *userName
-	conf.Net.SASL.Password = *passwd
-	conf.Net.SASL.Handshake = true
-	conf.Net.SASL.SCRAMClientGeneratorFunc = func() sarama.SCRAMClient { return &XDGSCRAMClient{HashGeneratorFcn: SHA256} }
-	conf.Net.SASL.Mechanism = sarama.SASLTypeSCRAMSHA256
+    //jsonEncoder := json.NewEncoder(*responseWriter)
+    conf := sarama.NewConfig()
+    conf.Producer.Retry.Max = 1
+    conf.Producer.RequiredAcks = sarama.WaitForAll
+    conf.Producer.Return.Successes = true
+    conf.Metadata.Full = true
+    conf.Version = sarama.V0_10_0_0
+
+    if *userName != "" && *passwd != "" {
+        conf.Net.SASL.Enable = true
+        conf.Net.SASL.User = *userName
+        conf.Net.SASL.Password = *passwd
+        conf.Net.SASL.Handshake = true
+    }
+
+    if *encription == "256" {
+        conf.Net.SASL.SCRAMClientGeneratorFunc = func() sarama.SCRAMClient { return &XDGSCRAMClient{HashGeneratorFcn: SHA256} }
+        conf.Net.SASL.Mechanism = sarama.SASLTypeSCRAMSHA256
+    } else if *encription == "512" {
+        conf.Net.SASL.SCRAMClientGeneratorFunc = func() sarama.SCRAMClient { return &XDGSCRAMClient{HashGeneratorFcn: SHA512} }
+        conf.Net.SASL.Mechanism = sarama.SASLTypeSCRAMSHA512
+    }
 	conf.ClientID = "rest2kafka"
 	splitBrokers := strings.Split(*brokers, ",")
 	producer, err := sarama.NewSyncProducer(splitBrokers, conf)
@@ -51,20 +60,6 @@ func processMessages(messages *[]MessageStructure, responseWriter *http.Response
 	} else {
 		log.Printf("Messages sent")
 	}
-	// for _, message := range *messages {
-	// 	encodeErr := jsonEncoder.Encode(&message.Message)
-	// 	if encodeErr != nil {
-	// 		panic(encodeErr)
-	// 	}
-	// 	str := string(message.Message)
-	// 	msg := &sarama.ProducerMessage{Topic: topic, Value: sarama.StringEncoder(str)}
-	// 	partition, offset, err := producer.SendMessage(msg)
-	// 	if err != nil {
-	// 		log.Printf("FAILED to send message: %s\n", err)
-	// 	} else {
-	// 		log.Printf("> message sent to partition %d at offset %d\n", partition, offset)
-	// 	}
-	// }
 }
 
 var requestHandler = func(w http.ResponseWriter, req *http.Request) {
